24631b9f3049771932595b00165084262bbcbd44
Vid Palcar <vidpalcar@outlook.com>
2022-09-23 14:27:26 +0200

    Added missing comment

--- 13-running-live-experiments-with-a-b-testing-and-feature-flags/project/starter/packages/monitoring/lib/src/remote_value_service.dart
+++ 13-running-live-experiments-with-a-b-testing-and-feature-flags/project/starter/packages/monitoring/lib/src/remote_value_service.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
+// TODO: import firebase_remote_config package
 
 /// Wrapper around [FirebaseRemoteConfig].
 class RemoteValueService {
-  // TODO: add an implementation of RemoteValueService class
+// TODO: add an implementation of RemoteValueService class
 }
